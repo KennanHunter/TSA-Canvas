5e29dc1a851b5cc03fc2c543b52fb3ef8faf91e3
Kennan Hunter <kennanhunter5@gmail.com>
2021-12-05 02:23:51 -0500

    docker + postgres

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 
+console.log("API Started: Connecting to Database");
+let connected = false;
+let retries = 5;
+console.log(!connected && retries);
 createConnection()
 	.then(async (connection) => {
 		console.log("Inserting a new user into the database...");
@@ -20,4 +24,6 @@ createConnection()
 			"Here you can setup and run express/koa/any other framework.",
 		);
 	})
-	.catch((error) => console.log(error));
+	.catch((error) => {
+		console.log(error);
+	});
