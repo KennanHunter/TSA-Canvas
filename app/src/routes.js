f943e847af50e228d0c28b2ac6574440acc972bd
Kennan Hunter <kennanhunter5@gmail.com>
2021-12-22 22:16:55 -0500

    login route

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,10 +1,12 @@
 import Home from "./routes/Home.svelte";
 import Assignment from "./routes/Assignment.svelte";
 import NotFound from "./routes/NotFound.svelte";
+import Login from "./routes/Login.svelte";
 
 export default {
 	"/": Home,
 	"/assignment/:id": Assignment,
+	"/login": Login,
 	// The catch-all route must always be last
 	"*": NotFound,
 };
