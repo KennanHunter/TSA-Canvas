539ce7b23b4582adb817110228681e36450eba7e
Kennan Hunter <kennanhunter5@gmail.com>
2022-02-24 21:17:34 -0500

    student and teacher relations working

--- api/src/graphql/entities/Class.ts
+++ api/src/graphql/entities/Class.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from "apollo-server";
 import { extendType, nonNull, objectType, stringArg } from "nexus";
 import { Context } from "../../context";
 
@@ -7,6 +8,36 @@ export const Class = objectType({
 		t.nonNull.string("id");
 		t.nonNull.string("name");
 		// t.nonNull.color("color");
+		t.nonNull.list.field("members", {
+			type: "User",
+			async resolve(parent, args, context: Context) {
+				return (
+					await context.prisma.class.findUnique({
+						where: {
+							id: parent.id,
+						},
+						include: {
+							members: true,
+						},
+					})
+				).members;
+			},
+		});
+		t.nonNull.list.field("teachers", {
+			type: "User",
+			async resolve(parent, args, context: Context) {
+				return (
+					await context.prisma.class.findUnique({
+						where: {
+							id: parent.id,
+						},
+						include: {
+							teachers: true,
+						},
+					})
+				).teachers;
+			},
+		});
 		t.nonNull.field("owner", {
 			type: "User",
 			resolve(parent, args, context) {
@@ -24,7 +55,7 @@ export const ClassFeed = extendType({
 	type: "Query",
 	definition(t) {
 		t.nonNull.list.nonNull.field("classFeed", {
-			type: "Class",
+			type: Class,
 			resolve(parent, args, context) {
 				return context.prisma.class.findMany();
 			},
@@ -49,6 +80,84 @@ export const ClassMutation = extendType({
 						name,
 						color,
 						owner: { connect: { id: context.userId } },
+					},
+				});
+
+				return newClass;
+			},
+		});
+		t.nonNull.field("addUser", {
+			type: Class,
+			args: {
+				classId: nonNull(stringArg()),
+				studentId: nonNull(stringArg()),
+			},
+			async resolve(parent, args, context: Context) {
+				const { classId, studentId } = args;
+
+				if (
+					(
+						await context.prisma.class
+							.findUnique({
+								where: {
+									id: classId,
+								},
+							})
+							.owner()
+					).id !== context.userId
+				) {
+					throw new AuthenticationError("User does not own class");
+				}
+
+				const newClass = context.prisma.class.update({
+					where: {
+						id: classId,
+					},
+					data: {
+						members: {
+							connect: {
+								id: studentId,
+							},
+						},
+					},
+				});
+
+				return newClass;
+			},
+		});
+		t.nonNull.field("addTeacher", {
+			type: Class,
+			args: {
+				classId: nonNull(stringArg()),
+				teacherId: nonNull(stringArg()),
+			},
+			async resolve(parent, args, context: Context) {
+				const { classId, teacherId } = args;
+
+				if (
+					(
+						await context.prisma.class
+							.findUnique({
+								where: {
+									id: classId,
+								},
+							})
+							.owner()
+					).id !== context.userId
+				) {
+					throw new AuthenticationError("User does not own class");
+				}
+
+				const newClass = context.prisma.class.update({
+					where: {
+						id: classId,
+					},
+					data: {
+						members: {
+							connect: {
+								id: teacherId,
+							},
+						},
 					},
 				});
 
