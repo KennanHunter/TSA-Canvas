ecad7fb569743fa291a3b17389da13e589ad35b2
Kennan Hunter <kennanhunter5@gmail.com>
2022-03-07 22:16:25 -0500

    app stuffs

--- app/src/routes/Home.svelte
+++
@@ -1,3 +0,0 @@
-<h1>Future Dashboard/Home page</h1>
-
-<a href="/#/assignment/test-assignment">for now just use this link </a>
---
+++ app/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Better Canvas</title>
+</svelte:head>
+
+<section />
+
+<style>
+</style>
