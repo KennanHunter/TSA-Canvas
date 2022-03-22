e695c0a77cbcda38d0077dfe2b4e8e7e693138af
Kennan Hunter <kennanhunter5@gmail.com>
2022-03-22 01:46:30 -0400

    api class entity

--- api/src/graphql/entities/Class.ts
+++ api/src/graphql/entities/Class.ts
@@ -121,29 +121,29 @@ export const ClassMutation = extendType({
 
 				return newClass;
 			},
-		// });
-		// t.nonNull.field("addMember", {
-		// 	type: Class,
-		// 	args: {
-		// 		classId: nonNull(stringArg()),
-		// 		studentId: nonNull(stringArg()),
-		// 	},
-		// 	async resolve(parent, args, context: Context) {
-		// 		const { classId, studentId } = args;
+			// });
+			// t.nonNull.field("addMember", {
+			// 	type: Class,
+			// 	args: {
+			// 		classId: nonNull(stringArg()),
+			// 		studentId: nonNull(stringArg()),
+			// 	},
+			// 	async resolve(parent, args, context: Context) {
+			// 		const { classId, studentId } = args;
 
-		// 		if (
-		// 			(
-		// 				await context.prisma.class
-		// 					.findUnique({
-		// 						where: {
-		// 							id: classId,
-		// 						},
-		// 					})
-		// 					.owner()
-		// 			).id !== context.userId
-		// 		) {
-		// 			throw new AuthenticationError("User does not own class");
-		// 		}
+			// 		if (
+			// 			(
+			// 				await context.prisma.class
+			// 					.findUnique({
+			// 						where: {
+			// 							id: classId,
+			// 						},
+			// 					})
+			// 					.owner()
+			// 			).id !== context.userId
+			// 		) {
+			// 			throw new AuthenticationError("User does not own class");
+			// 		}
 
 			// 	const newClass = context.prisma.class.update({
 			// 		where: {
