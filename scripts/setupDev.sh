db1ffb1f2536dea25e2f091dc18c3695d77253ae
Kennan Hunter <kennanhunter5@gmail.com>
2022-04-07 23:51:09 -0400

    nginx build factored out

--- scripts/setupDev.sh
+++ scripts/setupDev.sh
@@ -2,7 +2,7 @@
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[-1]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 
-cd $SCRIPT_DIRECTORY/../
+cd $SCRIPT_DIRECTORY/
 
 cd ../api
 yarn install
@@ -18,3 +18,4 @@ openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout localhost.ke
 
 cd ..
 cp .env.example .env
+docker-compose build
