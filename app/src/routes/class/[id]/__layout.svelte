0aac936a2c8d57308d6e0d415a152503836a3bdc
Kennan Hunter <kennanhunter5@gmail.com>
2022-04-18 22:35:01 -0400

    update class sidebar

--- app/src/routes/class/[id]/__layout.svelte
+++ app/src/routes/class/[id]/__layout.svelte
@@ -1,17 +1,56 @@
 <script>
 	import { page } from "$app/stores";
+	let baseUrl = "/class/" + $page.params.id + "/";
 </script>
 
-<menu>
-	<ul>
-		<li>Class Page</li>
-		<li>Grades</li>
+<div class="outer">
+	<menu>
+		<ul>
+			<a href={baseUrl}>
+				<li>Class Page</li>
+			</a>
+			<a href={baseUrl + "grades"}>
+				<li>Grades</li>
+			</a>
+			<a href={baseUrl + "memberlist"}>
+				<li>Class Members</li>
+			</a>
+			{#if false}
+				<li>Class Settings</li>
+			{/if}
+			<a href={baseUrl + "assignment"}><li>Assignments</li></a>
+		</ul>
+	</menu>
+	<div>
+		<slot />
+	</div>
+</div>
 
-		<li>Class Members</li>
-		<li>Class Settings</li>
-		<a href={"/class/" + $page.params.id + "/assignment/"}
-			><li>Assignments</li></a
-		>
-	</ul>
-</menu>
-<slot />
+<style lang="scss">
+	@import "../../../app.scss";
+	.outer {
+		display: grid;
+		grid-template-columns: 14em auto;
+	}
+	a {
+		all: unset;
+	}
+	menu {
+		padding: 0;
+		margin: 0;
+		height: 100%;
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+		border-right: 0.1em solid black;
+		margin-right: 3em;
+	}
+	li {
+		padding: 0.7em 0 0.7em 0.7em;
+	}
+	li:hover {
+		background-color: $secondary-color;
+		font-weight: 700;
+	}
+</style>
