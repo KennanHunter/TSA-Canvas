e70feb32dbe4bcb97926c4075ee85191d10c0862
Kennan Hunter <kennanhunter5@gmail.com>
2022-04-21 02:07:42 -0400

    fix dropdown styling when small

--- app/src/lib/header/Dropdown.svelte
+++ app/src/lib/header/Dropdown.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { logout } from "$lib/functions/logout";
+	import { fade } from "svelte/transition";
 
 	import Menu from "svelte-material-icons/Menu.svelte";
 	let dropdown: boolean;
@@ -7,15 +8,17 @@
 
 <div class="template">
 	<button
-		style="all: unset"
 		on:click={() => {
 			dropdown = !dropdown;
+			setTimeout(() => {
+				dropdown = false;
+			}, 5000);
 		}}
 	>
 		<Menu size="2.5em" />
 	</button>
 	{#if dropdown}
-		<ul>
+		<ul transition:fade={{ duration: 300 }}>
 			<a href="/class/"><li>View All Classes</li></a>
 			<a href="/settings/"> <li>Settings</li></a>
 			<li on:click={logout}>Logout</li>
@@ -31,6 +34,7 @@
 	ul {
 		position: fixed;
 		right: 1em;
+		top: 4em;
 		background-color: app.$background-color;
 		border: 0.3em app.$secondary-color solid;
 		border-radius: 1em;
@@ -45,5 +49,15 @@
 	li:hover {
 		background-color: app.$primary-color;
 		font-weight: 700;
+	}
+	button {
+		all: unset;
+	}
+	@media only screen and (max-width: 600px) {
+		button {
+			position: absolute;
+			top: 1em;
+			right: 1em;
+		}
 	}
 </style>
