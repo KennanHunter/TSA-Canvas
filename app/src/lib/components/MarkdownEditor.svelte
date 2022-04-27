c32c1608aba35b8c003826114c3f0467fe4f23d6
Kennan Hunter <kennanhunter5@gmail.com>
2022-04-27 04:58:52 -0400

    hope and pray

--- app/src/lib/components/MarkdownEditor.svelte
+++ app/src/lib/components/MarkdownEditor.svelte
@@ -1,15 +1,44 @@
-<script>
+<script lang="ts">
+	import { mutation } from "$lib/functions/query";
+
 	import { plugins } from "$lib/markdownPlugins";
 	import { Editor } from "bytemd";
+	import type { Image } from "mdast";
 
 	export let value = "";
 
 	function handleMarkdownChange(event) {
 		value = event.detail.value;
 	}
+
+	async function uploadFile(files: File[]): Promise<{ url }[]> {
+		let returnValue: { url: string }[];
+		files.forEach(async (file) => {
+			await mutation({
+				uploadFile: {
+					link: true,
+					file: {
+						id: true,
+					},
+				},
+			}).then((value) => {
+				fetch(value.uploadFile.link, {
+					method: "PUT",
+					body: file,
+				}).then(() => {
+					returnValue.push({
+						url: "/minio/upload/" + value.UploadFile.file.id,
+					});
+				});
+			});
+		});
+		return returnValue;
+	}
 </script>
 
-<Editor {value} {plugins} on:change={handleMarkdownChange} />
-
-<style>
-</style>
+<Editor
+	{value}
+	{plugins}
+	on:change={handleMarkdownChange}
+	uploadImages={uploadFile}
+/>
